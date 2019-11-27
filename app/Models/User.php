b05f8a99ee9ac6a2fd8eafcb30f619662ec2a034
ikay <oluudeh@gmail.com>
2019-11-27 19:34:00 +0100

    user jwtsubject

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
