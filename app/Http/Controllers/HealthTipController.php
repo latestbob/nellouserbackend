53c656a6d8248644fb676b5661b62ac0057fc436
ikay <oluudeh@gmail.com>
2019-12-29 18:33:50 +0100

    bug fix

--- app/Http/Controllers/HealthTipController.php
+++ app/Http/Controllers/HealthTipController.php
@@ -23,8 +23,6 @@ class HealthTipController extends Controller
 
     public function lastTip() {
         $tip = HealthTip::orderBy('created_at', 'desc')->first();
-        return [
-            'tip' => $tip
-        ];
+        return $tip;
     }
 }
