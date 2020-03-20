7b4a3b951901a2b53ae55655cc07d9422273dd95
wisdom.emenike <wisdom.emenike@imaxinacion.info>
2020-03-20 18:32:12 +0100

    Refactored admin and customer feed back controllers

--- app/Http/Controllers/Api/FeedBackController.php
+++ app/Http/Controllers/Api/FeedBackController.php
@@ -3,26 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Admin;
 use App\Models\Feedbacks;
-use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Validator;
-use Tymon\JWTAuth\Exceptions\TokenExpiredException;
-use Tymon\JWTAuth\Exceptions\TokenInvalidException;
-use Tymon\JWTAuth\Facades\JWTAuth;
-
 class FeedBackController extends Controller
 {
-
-    public function __construct()
-    {
-        Config::set('auth.providers', ['users' => [
-            'driver' => 'eloquent',
-            'model' => Admin::class,
-        ]]);
-    }
 
     public function create(Request $request) {
 
@@ -46,29 +31,5 @@ class FeedBackController extends Controller
             'message' => 'Feedback sent successfully',
             'data' => [$feedback]
         ];
-    }
-
-    public function getFeedbacks(Request $request) {
-
-        try {
-            $admin = JWTAuth::parseToken()->authenticate();
-        } catch (Exception $e) {
-            if ($e instanceof TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
-            }else if ($e instanceof TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired']);
-            }else{
-                return response()->json(['status' => 'Authorization Token not found']);
-            }
-        }
-
-        if (empty($request->facilityID)) {
-            return response(['msg' => ['Facility ID is missing']], 400);
-        }
-
-        $size = empty($request->size) ? 1 : $request->size;
-        $feedbacks = Feedbacks::where(['vendor_id' => $request->facilityID])
-            ->orderBy('created_at', 'desc')->paginate($size);
-        return $feedbacks;
     }
 }
