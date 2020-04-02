82b563e39519c8ee6b0db48894e87359f85d3929
wisdom.emenike <wisdom.emenike@imaxinacion.info>
2020-04-02 12:35:40 +0100

    added new column to carts table

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     const TAX_FEE = 0.05; //percentage of total price
-    protected $fillable = ['cart_uuid', 'drug_id', 'quantity', 'price', 'prescription', 'user_id', 'vendor_id'];
+    protected $fillable = ['cart_uuid', 'drug_id', 'quantity', 'price', 'prescription', 'prescribed_by', 'user_id', 'vendor_id'];
 
     public function order()
     {
