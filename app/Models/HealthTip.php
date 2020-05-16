2cce7bad72c60ef7ef99ce1ab71cd7529e570f3f
iamwizzdom <wisdomemenike70@gmail.com>
2020-05-16 22:06:50 +0100

    Created health tip module and migrations

--- app/Models/HealthTip.php
+++ app/Models/HealthTip.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class HealthTip extends Model
 {
-    protected $fillable = ['tip','month','day','year','tip_range', 'uuid', 'vendor_id'];
+//    protected $fillable = ['tip','month','day','year','tip_range', 'uuid', 'vendor_id'];
+    protected $fillable = ['uuid', 'title', 'body', 'vendor_id'];
+
+    protected $hidden = ['vendor_id'];
 
 }
