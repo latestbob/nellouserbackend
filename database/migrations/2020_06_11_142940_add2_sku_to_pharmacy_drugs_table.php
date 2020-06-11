fbfd1173b7d0b1f8a3bdda1e9bec13b31c618fde
Olu Udeh <oluudeh@gmail.com>
2020-06-11 06:33:21 -0700

    drug sku migration

--- database/migrations/2020_06_11_142940_add2_sku_to_pharmacy_drugs_table.php
+++ database/migrations/2020_06_11_142940_add2_sku_to_pharmacy_drugs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSkuToPharmacyDrugsTable extends Migration
+class Add2SkuToPharmacyDrugsTable extends Migration
 {
     /**
      * Run the migrations.
