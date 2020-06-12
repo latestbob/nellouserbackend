7b5623e6897d2f1f6b963f76e6522cdf396326c7
iamwizzdom <wisdomemenike70@gmail.com>
2020-06-12 09:31:23 +0100

    Minor modification on drug status

--- app/Http/Controllers/Api/DrugController.php
+++ app/Http/Controllers/Api/DrugController.php
@@ -18,15 +18,17 @@ class DrugController extends Controller
     {
         if (empty($request->search) && empty($request->category)) {
 
-            $drugs = PharmacyDrug::with('category')->where(
-                'status', true)->orderBy('name')->paginate();
+            $drugs = PharmacyDrug::with('category')
+//                ->where('status', true)
+                ->orderBy('name')->paginate();
 
         } else {
 
             $search = $request->search;
             $category = $request->category;
 
-            $drugs = PharmacyDrug::with('category')->where('status', true)
+            $drugs = PharmacyDrug::with('category')
+//                ->where('status', true)
                 ->when($category, function ($query, $category) {
                     $query->where('category_id', '=', "{$category}");
                 })->when($search, function ($query, $search) {
@@ -41,7 +43,9 @@ class DrugController extends Controller
     public function getDrug(Request $request)
     {
         return PharmacyDrug::with('category')->where(
-            [['uuid', '=', $request->uuid], ['status', '=', true]])->first();
+            [['uuid', '=', $request->uuid]
+//                , ['status', '=', true]
+            ])->first();
     }
 
     public function getDrugCategories(Request $request)
