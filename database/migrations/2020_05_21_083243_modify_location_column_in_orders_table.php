309c42bbf242d2b38d49fa05cc84da77577e45e7
Olu Udeh <oluudeh@gmail.com>
2020-08-31 03:39:42 -0700

    migration bug fix

--- database/migrations/2020_05_21_083243_modify_location_column_in_orders_table.php
+++ database/migrations/2020_05_21_083243_modify_location_column_in_orders_table.php
@@ -14,7 +14,7 @@ class ModifyLocationColumnInOrdersTable extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->unsignedBigInteger('pickup_location_id')->after('location_id')->nullable();
+            $table->unsignedBigInteger('pickup_location_id')->nullable();
             $table->foreign('pickup_location_id', 'orders_pickup_location_id_foreign')->references('id')
                 ->on('pharmacies')->onDelete('cascade')->onUpdate('cascade');
         });
