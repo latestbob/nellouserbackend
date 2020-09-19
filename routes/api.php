7f8e3f35d897ab7a8b363a4be6cff4f4e4580978
iamwizzdom <wisdomemenike70@gmail.com>
2020-09-19 17:55:44 +0100

    Created endpoint for contacting doctors

--- routes/api.php
+++ routes/api.php
@@ -84,7 +84,7 @@ Route::post('token/update', 'Api\AuthController@updateToken')->middleware('jwt.a
 
 
 
-Route::prefix('/admin')->group(function () {
+//Route::prefix('/admin')->group(function () {
 
 //    Route::prefix('/auth')->group(function (){
 //        Route::post('/login', 'Api\Admin\AuthController@loginAdmin')->name('login');
@@ -105,7 +105,7 @@ Route::prefix('/admin')->group(function () {
 //        Route::post('/drug/item/add-prescription', 'Api\Admin\OrderController@addPrescription');
 //    });
 
-});
+//});
 
 Route::prefix('/profile')->middleware('jwt.auth')->group(function() {
     Route::post('/update', 'Api\ProfileController@updateCustomer');
@@ -138,6 +138,7 @@ Route::get('/drug/categories', 'Api\DrugController@getDrugCategories');
 Route::get('/drug/{uuid}', 'Api\DrugController@getDrug');
 Route::get('/doctors', 'Api\DoctorController@fetchDoctors'); //->middleware('jwt.auth');
 Route::get('/doctor/{uuid}', 'Api\DoctorController@fetchDoctor'); //->middleware('jwt.auth');
+Route::post('/doctor/contact', 'Api\DoctorController@contactDoctor'); //->middleware('jwt.auth');
 Route::post('/doctor/rate', 'Api\DoctorController@rateDoctor')->middleware('jwt.auth');
 Route::get('/doctors/specializations', 'Api\DoctorController@fetchSpecializations');
 Route::get('/health-centers', 'HealthCenterController@index')->middleware('jwt.auth');
