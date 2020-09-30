4ed7836ef4096eded46a29d31958c8428aafb2cc
iamwizzdom <wisdomemenike70@gmail.com>
2020-09-30 17:23:07 +0100

    Bug fix

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -9,13 +9,13 @@ class Appointment extends Model
     protected $table = 'appointments';
     protected $fillable = ['uuid', 'description','user_uuid','status','reason','date','time','location','source','session_id','ref_no','center_uuid'];
 
-    public function center() 
+    public function center()
     {
         return $this->hasOne('App\Models\HealthCenter', 'uuid', 'center_uuid');
     }
 
-    public function user() 
+    public function user()
     {
-        return $this->belongsTo('App\Models\User', 'uuid', 'user_uuid');
+        return $this->belongsTo(User::class, 'user_uuid', 'uuid');
     }
 }
