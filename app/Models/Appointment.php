814821688d3c6d9b427f604ebb5fa72ccb94991f
Olu Udeh <oluudeh@gmail.com>
2020-12-07 01:13:47 -0800

    Add type to appointments

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     protected $table = 'appointments';
-    protected $fillable = ['uuid', 'description','user_uuid','status','reason','date','time','location','source','session_id','ref_no','center_uuid'];
+    protected $fillable = ['uuid', 'description','user_uuid','status','reason','date','time','location','source','session_id','ref_no','center_uuid', 'type'];
 
     public function center()
     {
