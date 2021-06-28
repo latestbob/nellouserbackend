d31a451d14dd3d1714178f0a16a0cf1a6e41c6f4
Olu Udeh <oluudeh@gmail.com>
2021-06-28 02:15:05 -0700

    checkout summary

--- routes/api.php
+++ routes/api.php
@@ -165,7 +165,7 @@ Route::prefix('/feedback')->group(function() {
 
 Route::prefix('/order')->group(function() {
     Route::post('/checkout', 'Api\OrderController@checkout');
-    Route::match(['get', 'post'], '/checkout/summary', 'Api\OrderController@checkoutSummary');
+    Route::post('/checkout/summary', 'Api\OrderController@checkoutSummary');
     Route::post('/drug/confirm-payment', 'Api\OrderController@confirmPayment');
     Route::post('/merge', 'Api\OrderController@mergeOrder');
     Route::post('/cancel', 'Api\OrderController@cancelOrder');
