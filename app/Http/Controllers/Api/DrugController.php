5184e389053f64eedc5b35406127f7c889617229
Ikay <oluudeh@gmail.com>
2021-11-03 15:36:38 +0100

    add drug prescription request validation fix

--- app/Http/Controllers/Api/DrugController.php
+++ app/Http/Controllers/Api/DrugController.php
@@ -296,6 +296,7 @@ class DrugController extends Controller
         return compact('status', 'msg', 'orderItems', 'size', 'userType');
     }
 
+
     public function drugOrderItemAction(Request $request)
     {
 
@@ -475,7 +476,7 @@ class DrugController extends Controller
     {
 
         $validator = Validator::make($request->all(), [
-            'uuid' => 'required|uuid|exists:carts,cart_uuid',
+            'uuid' => 'required|exists:carts,cart_uuid',
             'id' => 'required|integer',
             'file' => 'required|file|mimes:jpeg,jpg,png,pdf',
         ]);
